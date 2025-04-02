d355a955210aa53172101bc4512e4aa81ccd2350
Mahesh Kakunuri <maheshkakunuri9@gmail.com>
2025-04-02 18:03:54 +0530

    updated about,map and stories

---
+++ src/data.js
@@ -0,0 +1,99 @@
+const testimonials = [
+    {
+      name: "Patient G. Sridhar Raju",
+      category: "General",
+      feedback:
+        "Srinivas sir and Dr Suhasini madam completed all 12 sessions efficiently. While 2 to 3 sessions had no counseling, the remaining sessions helped me a lot to overcome negative thoughts and feel more energetic.",
+    },
+    {
+      name: "Anonymous",
+      category: "OCD",
+      feedback:
+        "rTMS treatment helped me to overcome some of my OCD problems. Dr. Srinivas was very patient and has given me the best consultation ever, helping me manage my OCD triggers. Dr. Suhasini Geetha was soft-spoken and understood my problems, suggesting rTMS.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "I had issues due to health anxiety, but thanks to Asha Neuro Modulation Clinics, Dilsukhnagar, and Psychologist Srinivas and Psychiatrist Suhasini, they guided me with good techniques and motivation. Today I am 60% cured after just 2 weeks and highly recommend them. I am confident to recover fully in another month.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "The doctor was very good at her therapy. She received and counseled me in a positive and effective manner. I am feeling better after visiting this clinic.",
+    },
+    {
+      name: "Anonymous",
+      category: "OCD",
+      feedback:
+        "Deep TMS therapy was helpful for my depression. Dr. Suhasini helped me a lot, and she is a very good doctor. I highly recommend this clinic.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "ANC branch provides very good facilities, treatments, and services. The staff is friendly, and both the psychologist and doctor are very supportive. Thank you, Srinivas sir and Dr. Suhasini madam. I am very happy with this neuromodulation treatment.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "Dr. Suhasini and Srinivas sir guided me with good techniques and motivation. Srinivas sir made me very comfortable during the treatment. I felt much better after the treatment.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "My anxiety and depression improved by 70% in 3 months. Dr. Suhasini is a very good doctor. The clinic has a nice ambiance and friendly staff. I highly recommend this doctor.",
+    },
+    {
+      name: "Anonymous",
+      category: "OCD",
+      feedback:
+        "rTMS (Repetitive Transcranial Magnetic Stimulation) treatment is highly beneficial for managing OCD. It involves non-invasive brain stimulation to alleviate symptoms. Additionally, receiving excellent counseling from a psychologist like Mr. Srinivas and effective prescriptions from Dr. Suhasini enhances the overall treatment experience.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "Professional staff, clean office, and a friendly environment. I appreciate the staff for their patience and support. Thank you, Psychologist Mr. Srinivas and Dr. Suhasini, for your presence and patience with us (family members), answering all our questions without hesitation. Wishing for a smooth recovery for all those in care.",
+    },
+    {
+      name: "Anonymous",
+      category: "OCD",
+      feedback:
+        "rTMS treatment helped me to overcome my OCD problems. Dr. Srinivas was very patient and gave me the best consultation ever. He helped me manage my OCD triggers, and Dr. Suhasini Geetha was soft-spoken and suggested rTMS.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "Very convenient location, friendly doctor, and cordial staff. DTMS treatment is very effective for OCD and depression.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "Good doctor, well-trained staff, nice ambiance, and good infrastructure. I am very happy with my treatment and doing well.",
+    },
+    {
+      name: "Anonymous",
+      category: "General",
+      feedback:
+        "Very good doctor, well-trained staff, nice ambiance, and good infrastructure. I am very happy with my treatment and doing well.",
+    },
+    {
+      name: "Anonymous",
+      category: "Stress",
+      feedback:
+        "I am so confused and worried, triggering every second. I am so depressed seeing repeated things around me. I have always done everything for others, but they never supported me when I was struggling. I feel totally addicted and cry easily from stress. What is my problem? Please suggest something.",
+    },
+    {
+      name: "Anonymous",
+      category: "Counselling",
+      feedback:
+        "The doctor is always late. This is my second visit, and the staff isn't friendly. They have no clue when the doctor would arrive.",
+    },
+  ];
+export default  testimonials
